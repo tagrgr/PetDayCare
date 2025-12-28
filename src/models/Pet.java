45d30e24a1776e4d6737c2bb8af4e450ce1a8184
Tiago de Gregori <tiago.gregori@live.com>
2025-12-28 19:13:36 +0000

    working on pet.java constructors

--- src/models/Pet.java
+++ src/models/Pet.java
@@ -1,22 +1,28 @@
 package models;
+import utils.Utilities;
 
 // abstract define shared behaviour, ideal for our super class
 public abstract class Pet {
     // -------Fields-------
-    private int id = 1000;
+    // Static ID generation
+    private static int nextId = 1000;
+
+    private int id;
     private String name = "";
     private String owner = "";
     // a week has 7 days so our array will have 7 value's.
     private boolean[] daysAttending = new boolean[] {false, false, false, false, false, false, false};
     private char sex = 'f';
     private int age = 0;
-    // Static ID generation
-    private static int nextId = 1000;
 
     // -------Constructors-------
-    public Pet(int id, String name, String owner, char sex, int age) {
+    public Pet(String name, String owner, char sex, int age) {
+        this.id = nextId++;
+        // truncate will enforce max length, use this. for validation, store in the object
+        this.name = Utilities.truncateString(name, 30);
 
-        
+
+
     }
 
 
